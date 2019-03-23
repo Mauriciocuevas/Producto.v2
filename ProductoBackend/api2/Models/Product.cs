f00cedaa9e4ddee0bb082439210bf0029f37ab50
mauricio alejandro cuevas vidaurre <ma-uri-cio12@hotmail.com>
2019-03-23 11:26:34 -0400

    Api2

---
+++ ProductoBackend/api2/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api2.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductoID { get; set; }
+        [Required]
+        [Display(Name = "Nombre del Producto")]
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
